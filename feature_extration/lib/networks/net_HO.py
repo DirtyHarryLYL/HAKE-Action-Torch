448d5a860e820a00c8bd784db41be9ed07ee9cf5
Xinpeng Liu <43176336+Foruck@users.noreply.github.com>
2020-10-27 22:02:28 +0800

    Add files via upload

---
+++ feature_extration/lib/networks/net_HO.py
@@ -0,0 +1,175 @@
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
+import tensorflow as tf
+import tensorflow.contrib.slim as slim
+from tensorflow.contrib.slim import arg_scope
+from tensorflow.contrib.slim.python.slim.nets import resnet_utils
+from tensorflow.contrib.slim.python.slim.nets import resnet_v1
+from tensorflow.contrib.layers.python.layers import layers
+from tensorflow.contrib.layers.python.layers import regularizers
+from tensorflow.python.ops import nn_ops
+from tensorflow.contrib.layers.python.layers import initializers
+from tensorflow.python.framework import ops
+
+from ult.config import cfg
+from ult.visualization import draw_bounding_boxes_HOI
+
+import numpy as np
+import ipdb
+
+def resnet_arg_scope(is_training=True,
+                     weight_decay=cfg.TRAIN.WEIGHT_DECAY,
+                     batch_norm_decay=0.997,
+                     batch_norm_epsilon=1e-5,
+                     batch_norm_scale=True):
+    batch_norm_params = {
+        'is_training': False,
+        'decay': batch_norm_decay,
+        'epsilon': batch_norm_epsilon,
+        'scale': batch_norm_scale,
+        'trainable': False,
+        'updates_collections': ops.GraphKeys.UPDATE_OPS
+    }
+    with arg_scope(
+        [slim.conv2d, slim.fully_connected],
+        weights_regularizer = tf.contrib.layers.l2_regularizer(cfg.TRAIN.WEIGHT_DECAY),
+        weights_initializer = slim.variance_scaling_initializer(),
+        biases_regularizer  = tf.contrib.layers.l2_regularizer(cfg.TRAIN.WEIGHT_DECAY), 
+        biases_initializer  = tf.constant_initializer(0.0),
+        trainable           = is_training,
+        activation_fn       = tf.nn.relu,
+        normalizer_fn       = slim.batch_norm,
+        normalizer_params   = batch_norm_params):
+        with arg_scope([slim.batch_norm], **batch_norm_params) as arg_sc:
+            return arg_sc
+
+class ResNet50():
+    def __init__(self):
+        self.visualize = {}
+        self.intermediate = {}
+        self.predictions = {}
+        self.score_summaries = {}
+        self.event_summaries = {}
+        self.train_summaries = []
+        self.losses = {}
+
+        self.image       = tf.placeholder(tf.float32, shape=[1, None, None, 3], name = 'image')
+        self.H_boxes     = tf.placeholder(tf.float32, shape=[None, 5], name = 'H_boxes')
+        self.O_boxes     = tf.placeholder(tf.float32, shape=[None, 5], name = 'O_boxes')
+        self.num_fc      = 1024
+        self.scope       = 'resnet_v1_50'
+        self.stride      = [16, ]
+        self.lr          = tf.placeholder(tf.float32)
+        self.features    = {}
+        if tf.__version__ == '1.1.0':
+            self.blocks     = [resnet_utils.Block('block1', resnet_v1.bottleneck,[(256,   64, 1)] * 2 + [(256,   64, 2)]),
+                               resnet_utils.Block('block2', resnet_v1.bottleneck,[(512,  128, 1)] * 3 + [(512,  128, 2)]),
+                               resnet_utils.Block('block3', resnet_v1.bottleneck,[(1024, 256, 1)] * 5 + [(1024, 256, 1)]),
+                               resnet_utils.Block('block4', resnet_v1.bottleneck,[(2048, 512, 1)] * 3),
+                               resnet_utils.Block('block5', resnet_v1.bottleneck,[(2048, 512, 1)] * 3)]
+        else:
+            from tensorflow.contrib.slim.python.slim.nets.resnet_v1 import resnet_v1_block
+            self.blocks = [resnet_v1_block('block1', base_depth=64,  num_units=3, stride=2), 
+                           resnet_v1_block('block2', base_depth=128, num_units=4, stride=2),
+                           resnet_v1_block('block3', base_depth=256, num_units=6, stride=1), 
+                           resnet_v1_block('block4', base_depth=512, num_units=3, stride=1),
+                           resnet_v1_block('block5', base_depth=512, num_units=3, stride=1)]
+
+    def build_base(self):
+        with tf.variable_scope(self.scope, self.scope):
+            net = resnet_utils.conv2d_same(self.image, 64, 7, stride=2, scope='conv1') 
+            net = tf.pad(net, [[0, 0], [1, 1], [1, 1], [0, 0]])
+            net = slim.max_pool2d(net, [3, 3], stride=2, padding='VALID', scope='pool1')
+
+        return net
+
+    def image_to_head(self, is_training):
+        with slim.arg_scope(resnet_arg_scope(is_training=False)):
+            net    = self.build_base()
+            net, _ = resnet_v1.resnet_v1(net,
+                                         self.blocks[0:cfg.RESNET.FIXED_BLOCKS], 
+                                         global_pool=False,
+                                         include_root_block=False,
+                                         scope=self.scope)
+        with slim.arg_scope(resnet_arg_scope(is_training=is_training)):
+            head, _ = resnet_v1.resnet_v1(net,
+                                          self.blocks[cfg.RESNET.FIXED_BLOCKS:-2], 
+                                          global_pool=False,
+                                          include_root_block=False,
+                                          scope=self.scope)
+        return head
+
+    def res5(self, pool5_H, pool5_O, is_training, name):
+        with slim.arg_scope(resnet_arg_scope(is_training=is_training)):
+
+            fc7_H, _ = resnet_v1.resnet_v1(pool5_H, 
+                                           self.blocks[-2:-1],
+                                           global_pool=False,
+                                           include_root_block=False,
+                                           reuse=False,
+                                           scope=self.scope)
+            fc7_H = tf.reduce_mean(fc7_H, axis=[1, 2])
+
+            fc7_O, _ = resnet_v1.resnet_v1(pool5_O,
+                                       self.blocks[-1:],
+                                       global_pool=False,
+                                       include_root_block=False,
+                                       reuse=False,
+                                       scope=self.scope)
+            fc7_O = tf.reduce_mean(fc7_O, axis=[1, 2])
+        
+        return fc7_H, fc7_O
+
+    def crop_pool_layer(self, bottom, rois, name):
+        with tf.variable_scope(name) as scope:
+
+            batch_ids    = tf.squeeze(tf.slice(rois, [0, 0], [-1, 1], name="batch_id"), [1])
+            bottom_shape = tf.shape(bottom)
+            height       = (tf.to_float(bottom_shape[1]) - 1.) * np.float32(self.stride[0])
+            width        = (tf.to_float(bottom_shape[2]) - 1.) * np.float32(self.stride[0])
+            x1 = tf.slice(rois, [0, 1], [-1, 1], name="x1") / width
+            y1 = tf.slice(rois, [0, 2], [-1, 1], name="y1") / height
+            x2 = tf.slice(rois, [0, 3], [-1, 1], name="x2") / width
+            y2 = tf.slice(rois, [0, 4], [-1, 1], name="y2") / height
+
+            bboxes        = tf.stop_gradient(tf.concat([y1, x1, y2, x2], axis=1))
+            if cfg.RESNET.MAX_POOL:
+                pre_pool_size = cfg.POOLING_SIZE * 2
+                crops = tf.image.crop_and_resize(bottom, bboxes, tf.to_int32(batch_ids), [pre_pool_size, pre_pool_size], name="crops")
+                crops = slim.max_pool2d(crops, [2, 2], padding='SAME')
+            else:
+                crops = tf.image.crop_and_resize(bottom, bboxes, tf.to_int32(batch_ids), [cfg.POOLING_SIZE, cfg.POOLING_SIZE], name="crops")
+        return crops
+
+    def build_network(self, is_training):
+        initializer = tf.random_normal_initializer(mean=0.0, stddev=0.01)
+
+        head       = self.image_to_head(is_training)
+        pool5_H    = self.crop_pool_layer(head, self.H_boxes, 'Crop_H')
+        pool5_O    = self.crop_pool_layer(head, self.O_boxes, 'Crop_O')
+
+        fc7_H, fc7_O = self.res5(pool5_H, pool5_O, is_training, 'Crop_R')
+        
+        self.features['H']  = fc7_H
+        self.features['O']  = fc7_O
+        
+        return
+
+
+    def create_architecture(self, is_training):
+
+        self.build_network(is_training)
+
+        layers_to_output = {}
+
+        return layers_to_output
+
+
+    def get_HO(self, sess, image, blobs):
+        feed_dict = {self.image: image, self.H_boxes: blobs['H_boxes'], self.O_boxes: blobs['O_boxes']}
+        fc7_H, fc7_O = sess.run([self.features['H'], self.features['O']], feed_dict=feed_dict)
+        return fc7_H, fc7_O
+        
+    
